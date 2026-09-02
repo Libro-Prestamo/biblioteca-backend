bf5054f725762b9affd2a351f8531c3c2ba7afc1
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-09-01 22:29:54 -0400

    fix: agrega configuracion CORS que faltaba en el commit anterior

--- src/main/java/cl/duoc/dsy1107/biblioteca_backend/security/SecurityConfig.java
+++ src/main/java/cl/duoc/dsy1107/biblioteca_backend/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package cl.duoc.dsy1107.biblioteca_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
@@ -12,7 +11,13 @@ import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 
@@ -23,6 +28,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+            .cors(withDefaults())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/public/**").permitAll()
                 .requestMatchers("/api/write/**").hasAuthority("SCOPE_prestamo.write")
@@ -58,5 +64,17 @@ public class SecurityConfig {
         return converter;
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
 
 }
