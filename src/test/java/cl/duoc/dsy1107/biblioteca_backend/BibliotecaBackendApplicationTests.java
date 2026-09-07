a1aecdc9585b4240cd052b71e3fa2348f5fc5c3a
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-09-07 14:55:57 -0300

    chore: agrega estructura de tests

---
+++ src/test/java/cl/duoc/dsy1107/biblioteca_backend/BibliotecaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1107.biblioteca_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
