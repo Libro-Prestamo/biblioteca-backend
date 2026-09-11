e9e95287358e0b81221af1d33b3d3bea057aea4e
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-09-11 20:24:55 -0300

    test: agrega pruebas basicas de seguridad y ajustes de configuracion para deploy

--- src/main/java/cl/duoc/dsy1107/biblioteca_backend/security/SecurityConfig.java
+++ src/main/java/cl/duoc/dsy1107/biblioteca_backend/security/SecurityConfig.java
@@ -67,7 +67,11 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://d1hfr6439g58qk.cloudfront.net"
+            
+        ));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
